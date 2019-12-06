f312909a492d921ede4f195d51a3e1f5b0e174cb
ekachner <ekibam@gmail.com>
2019-12-05 21:14:56 -0700

    deleted commented irregularities

--- answer.js
+++ answer.js
@@ -4,10 +4,6 @@ function beerOnTheWall(n){
     for(let i = n; i > 0; i--){
         let x = i;
         let y = i - 1;
-        // if(i === 1){
-        //     console.log(`${x} bottle of beer on the wall, ${x} bottle of beer...
-        //     take one down, pass it around, ${y} bottles of beer on the wall...`);
-        // }
         console.log(`${x} bottles of beer on the wall, ${x} bottles of beer...
         take one down, pass it around, ${y} bottles of beer on the wall...`);
     }
